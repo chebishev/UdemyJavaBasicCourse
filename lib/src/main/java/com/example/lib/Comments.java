6544f6c675f009b086fca4be7fcdc541bb0e3229
chebishev <atanas.chebishev@gmail.com>
2023-08-26 19:41:21 +0300

    initial commit The Complete Android 14 Developer Course - JavaBasicSection

---
+++ lib/src/main/java/com/example/lib/Comments.java
@@ -0,0 +1,11 @@
+package com.example.lib;
+
+public class Comments {
+
+    // comment
+
+    /*
+    multiline
+    comment
+     */
+}
---
+++ lib/src/main/java/com/example/lib/Variables.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 25;
+
+        age = 40;
+
+        System.out.println("My age: " + age);
+
+    }
+}
